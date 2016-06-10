eb4aa99a7c6f2f708fd8c8499fbb42291da6f044
Saim <saim.khan@colorado.edu>
2016-06-09 19:59:43 -0700

    Update default.rb

--- cookbooks/apt/recipes/default.rb
+++ cookbooks/apt/recipes/default.rb
@@ -5,6 +5,7 @@
 # Copyright 2016, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
+# Ensure apt-get is up to date before proceeding with the runlist
 
 execute "apt-get update" do
   command "apt-get update"
